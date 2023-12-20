24ca66db608a00e7ca690351fdc5086c9d6bf09f
Hossein Abbaszadegan <habbaszadegan@gmail.com>
2023-12-20 09:45:17 +0330

    Created event listeners
    Clicking numbered buttons populates the display. Need to do the same
    for operators.

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ let number1;
 let operator;
 let number2;
 
+const display = document.querySelector('#display');
+const clear = document.querySelector('#clear');
+const del = document.querySelector('#delete');
+const nums = document.querySelectorAll('.nums');
+const operat3r = document.querySelectorAll('.operator');
+
 
 function operate (chosenOperator, num1, num2) {
     switch (chosenOperator) {
@@ -22,3 +28,13 @@ function operate (chosenOperator, num1, num2) {
             return NaN; // or return null or undefined depending on your desired behavior
     }
 }
+
+nums.forEach(button => {
+    button.addEventListener('click', () => {
+        display.textContent = button.textContent;
+    })
+})
+
+// create an event listener and assign it the corresponding number click to populate the display with.
+// create on eevent listener, loop over all items, if the string matches the current iteration populate 
+// that value into the display
