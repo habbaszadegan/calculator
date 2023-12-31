c7aee094cbf9b8cce74e2a618bcb94a0fceada48
Hossein Abbaszadegan <habbaszadegan@gmail.com>
2023-12-31 17:16:57 +0330

    Cleaned up code, made small adjustments to equals
    Clicking equals now clears both nextNum and currentNum, preventing
    unpredictable math operations.

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ let operator = '';
 let nextNum = '';
 let displayValue = '';
 let clearedDisplayForOperator = false;
-// let decimalCheck = false;
 
 let display = document.querySelector('#display');
 const clear = document.querySelector('#clear');
@@ -11,11 +10,9 @@ const del = document.querySelector('#delete');
 const nums = document.querySelectorAll('.nums');
 const operat3r = document.querySelectorAll('.operator');
 const equals = document.querySelector('.equals');
-const buttons = document.querySelectorAll('button');
 
 
 function operate (chosenOperator, num1, num2) {
-    let result;
     a = Number(num1);
     b = Number(num2);
     switch (chosenOperator) {
@@ -51,11 +48,6 @@ nums.forEach(button => {
                 display.textContent = '';
                 clearedDisplayForOperator = true; // Set flag to prevent further clearing
             }
-        // } else if (button === '.') {
-        //     if (!decimalCheck) {
-        //         display.textContent = display.textContent.slice(0, -1);
-        //         decimalCheck = true;
-        //     }
         }
         display.textContent += button.textContent;
     })
@@ -79,6 +71,7 @@ equals.addEventListener('click', () => {
     if (currentNum && operator && nextNum) {
         operate(operator, currentNum, nextNum);
     }
+    currentNum = '';
     nextNum = '';
 })
 
@@ -109,21 +102,3 @@ function whichNum(current, next) {
 function round(value) {
     return Number(Math.round(value+'e3')+'e-3');
 }
-
-//step 1: update display value so the numbers follow each other instead of overwriting each other
-    //step 1a: write separate function for step 1
-//step 2: acknowledge the operator input is the end of number input for num1 and we've moved on to num2
-//step 3: repeat step 1 for num2
-    // step 3a: if the operator is already set, run the operation and continue collecting numbers.
-//step 4: take in our three stored variables and initiate operate when clicking '='.
-//step 5: if there is an operator already stored in our variable, run the operate() function and replace
-//currentNum with the result of the function
-
-
-// Make the calculator work! You’ll need to store the first number and second number that are input into the 
-// calculator, utilize the operator that the user selects, and then operate() on the two numbers when the user 
-// presses the “=” key.
-// You should already have the code that can populate the display, so once operate() has been called, update the 
-// display with the ‘solution’ to the operation.
-// This is the hardest part of the project. You need to figure out how to store all the values and call the operate 
-// function with them. Don’t feel bad if it takes you a while to figure out the logic.
